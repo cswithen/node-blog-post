e0f772206b92a4f6d34c8feb861a3e47dc41c856
Cody Swithenbank <cody.swithenbank@gmail.com>
2022-06-23 13:52:35 -0700

    feat(redis): added redis for blog route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,34 +1,55 @@
-const mongoose = require('mongoose')
-const authenticate = require('@middleware/authenticate')
+const mongoose = require("mongoose");
+const authenticate = require("@middleware/authenticate");
 
-const Blog = mongoose.model('Blog')
+const Blog = mongoose.model("Blog");
 
-module.exports = app => {
-  app.get('/api/blogs/:id', authenticate, async (req, res) => {
+module.exports = (app) => {
+  app.get("/api/blogs/:id", authenticate, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
-    })
-    res.send(blog)
-  })
+      _id: req.params.id,
+    });
+    res.send(blog);
+  });
 
-  app.get('/api/blogs', authenticate, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id })
-    res.send(blogs)
-  })
+  app.get("/api/blogs", authenticate, async (req, res) => {
+    const redis = require("redis");    
+    const redusUrl = "redis://127.0.0.1:6379";
+    const client = redis.createClient(redusUrl);
+    const util = require('util')
+    // removes callback requirement
+    client.get = util.promisify(client.get);
 
-  app.post('/api/blogs', authenticate, async (req, res) => {
-    const { title, content } = req.body
+    // do we have any cached data related to this query
+    const cachedBlogs = await client.get(req.user.id);
+    
+    // if yes, respond to request immediately
+    if (cachedBlogs) {
+      console.log('SERVING FROM CACHE')
+      return res.send(JSON.parse(cachedBlogs));
+    }
+    
+    // if no, need to respond to request and update our cache
+    const blogs = await Blog.find({ _user: req.user.id });
+   
+    console.log('SERVING FROM MONGODB')
+    res.send(blogs);
+    
+    client.set(req.user.id, JSON.stringify(blogs));
+  });
+
+  app.post("/api/blogs", authenticate, async (req, res) => {
+    const { title, content } = req.body;
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
-    })
+      _user: req.user.id,
+    });
     try {
-      await blog.save()
-      res.send(blog)
+      await blog.save();
+      res.send(blog);
     } catch (err) {
-      res.send(400, err)
+      res.send(400, err);
     }
-  })
-}
+  });
+};
