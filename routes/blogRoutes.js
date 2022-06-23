3c41cc3db006114a08d211440bedac8fb022aedd
Cody Swithenbank <cody.swithenbank@gmail.com>
2022-06-23 16:14:44 -0700

    feat(redis): hydrating models
    allows developers to only toggle caches on certain queries

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -13,29 +13,9 @@ module.exports = (app) => {
   });
 
   app.get("/api/blogs", authenticate, async (req, res) => {
-    const redis = require("redis");    
-    const redusUrl = "redis://127.0.0.1:6379";
-    const client = redis.createClient(redusUrl);
-    const util = require('util')
-    // removes callback requirement
-    client.get = util.promisify(client.get);
-
-    // do we have any cached data related to this query
-    const cachedBlogs = await client.get(req.user.id);
-    
-    // if yes, respond to request immediately
-    if (cachedBlogs) {
-      console.log('SERVING FROM CACHE')
-      return res.send(JSON.parse(cachedBlogs));
-    }
-    
-    // if no, need to respond to request and update our cache
-    const blogs = await Blog.find({ _user: req.user.id });
+   const blogs = await Blog.find({ _user: req.user.id }).cache()
    
-    console.log('SERVING FROM MONGODB')
-    res.send(blogs);
-    
-    client.set(req.user.id, JSON.stringify(blogs));
+   res.send(blogs)
   });
 
   app.post("/api/blogs", authenticate, async (req, res) => {
